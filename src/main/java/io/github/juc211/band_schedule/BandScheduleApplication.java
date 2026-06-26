b7b320b225a2e2c970c43a8bf871ee99ea00fed8
juc211 <jungguechae0211@naver.com>
2026-06-26 18:50:47 +0900

    First Commit

---
+++ src/main/java/io/github/juc211/band_schedule/BandScheduleApplication.java
@@ -0,0 +1,13 @@
+package io.github.juc211.band_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/juc211/band_schedule/BandScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juc211.band_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
