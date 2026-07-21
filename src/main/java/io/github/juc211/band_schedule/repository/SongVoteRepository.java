64e339b96085a245a3a563aa28ac42f9a1206fd8
juc211 <jungguechae0211@naver.com>
2026-07-21 20:03:53 +0900

    1

--- src/main/java/io/github/juc211/band_schedule/repository/SongVoteRepository.java
+++ src/main/java/io/github/juc211/band_schedule/repository/SongVoteRepository.java
@@ -3,12 +3,15 @@ package io.github.juc211.band_schedule.repository;
 import io.github.juc211.band_schedule.domain.SongVote;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SongVoteRepository extends JpaRepository<SongVote, Long> {
 
 	Optional<SongVote> findBySongRequestIdAndVoterMemberId(Long songRequestId, Long voterMemberId);
 
+	@EntityGraph(attributePaths = {"songRequest", "voterMember"})
 	List<SongVote> findBySongRequestIdOrderByIdAsc(Long songRequestId);
 
 	void deleteBySongRequestId(Long songRequestId);
--- src/main/java/io/github/juc211/band_schedule/repository/TeamRepository.java
+++ src/main/java/io/github/juc211/band_schedule/repository/TeamRepository.java
@@ -2,10 +2,13 @@ package io.github.juc211.band_schedule.repository;
 
 import io.github.juc211.band_schedule.domain.Team;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
+	@EntityGraph(attributePaths = {"performance"}) //팀 데이터를 가져올 때 공연(performance)데이터까지 한번에 가져와서 N+1문제 해결
 	List<Team> findByPerformanceIdOrderByIdAsc(Long performanceId);
 
 	void deleteByPerformanceId(Long performanceId);
